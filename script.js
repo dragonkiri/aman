1941b2c5e14cab0617dcbf3027e6c05edbb1a7fd
dragonkiri <Raphaelmarquesmelo@gmail.com>
2025-05-09 07:58:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function comecar(){
+    document.getElementById("ela").style = "animation-name:voltar;"
+    document.getElementById("sobre").style = "animation-name:subir;"
+    document.getElementById("aprender").style = "animation-name:descer;"
+   
+}
+function sobre(){
+    document.getElementById("ela").style = "animation-name:desfilar;"
+    document.getElementById("sobre").style = "animation-name:descer;"
+    document.getElementById("aprender").style = "animation-name:subir;"
+   
+}
